07e8e51fb4a5bd9bf7f6d4b9593718821055800a
Lucas <58938251+lucas-in4it@users.noreply.github.com>
2019-12-23 11:02:31 +0100

    Feature/tfmodule (#2)
    * terraform module for ecs-upgrade

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "AWS_REGION" {}
+
+variable "ECS_CLUSTER" {}
+
+variable "LAUNCH_TEMPLATES" {}
+
+variable "DEBUG" {
+  type    = string
+  default = true
+}
+
+variable "ECS_ASG" {}
+
+variable "IMAGE" {
+  default = "in4it/ecs-upgrade:0.0.1"
+}
+
+variable "ECS_VPC_ID" {}
+
+# List of subnets
+variable "ECS_SUBNETS" {
+  type = list(string)
+}
+
+variable "EC2_IAM_ROLE_ARN" {}
+
+variable "EGRESS_CIDR" {
+  default = "0.0.0.0/0"
+}
